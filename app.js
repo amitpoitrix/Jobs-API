feae1323959d2e78694a605dbc1a7c1a16ce57ac
amitpoitrix <amit.poitrix@gmail.com>
2022-11-16 19:04:00 +0530

    Added swagger ui

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
+// Importing Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const express = require('express');
 const app = express();
 
@@ -26,19 +31,25 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 
 
 // Middleware
-app.set('trust proxy', 1);  // 
+app.set('trust proxy', 1);
 app.use(
     rateLimiter({
         windowMs: 15 * 60 * 1000, // 15 minutes
         max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-        standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-        legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+        // standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+        // legacyHeaders: false, // Disable the `X-RateLimit-*` headers
     })
 );
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
+
+// Swagger file
+app.get('/', (req, res) => {
+    res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 //Routes
 app.use('/api/v1/auth', authRouter);
