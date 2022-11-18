cb04ada08cdf5701b16bd8e76c4fa3e7335d673d
amitpoitrix <amit.poitrix@gmail.com>
2022-11-18 11:21:23 +0530

    Added some changes

--- app.js
+++ app.js
@@ -40,16 +40,15 @@ app.use(
         // legacyHeaders: false, // Disable the `X-RateLimit-*` headers
     })
 );
-app.use(express.json());
-app.use(helmet());
-app.use(cors());
-app.use(xss());
-
 // Swagger file
 app.get('/', (req, res) => {
     res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
 });
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 //Routes
 app.use('/api/v1/auth', authRouter);
